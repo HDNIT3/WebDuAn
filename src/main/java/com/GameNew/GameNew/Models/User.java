4c0ae2d303788fc791fbba43c9cb454ea845c337
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-08-26 20:00:13 +0700

    dang lam jwt

---
+++ src/main/java/com/GameNew/GameNew/Models/User.java
@@ -0,0 +1,20 @@
+package com.GameNew.GameNew.Models;
+
+import lombok.*;
+
+//@Setter
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private String username;
+//    
+//    @Column(name = "password")
+//    private String password;
+//    
+//    @Column(name = "role")
+//    private String role;
+}
