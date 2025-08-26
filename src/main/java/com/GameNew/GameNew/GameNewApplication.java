0eebb61d1b2e8c742707bc0732745cfa4d1427a4
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-08-26 18:25:08 +0700

    git first

---
+++ src/main/java/com/GameNew/GameNew/GameNewApplication.java
@@ -0,0 +1,13 @@
+package com.GameNew.GameNew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GameNew/GameNew/GameNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GameNew.GameNew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
